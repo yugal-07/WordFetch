8aa4a3ae99a92083e198d8e9f98d3d30a48fc2dc
yugal <patilyugal2007@gmail.com>
2026-07-09 23:07:41 +0530

    now capable for Xorg and Wayland

--- dictionary.py
+++ dictionary.py
@@ -2,12 +2,15 @@ import asyncio,requests,re,subprocess
 from desktop_notifier import DesktopNotifier
 
 #reading data from primary selection...
-#using wl-clipboard for wayland
-output_from_sp=subprocess.run(["wl-paste","--primary"],capture_output=True, text=True)
-input_word=str(output_from_sp.stdout)
-word="".join(input_word.splitlines()).strip()           #removing \n and blank spaces
-word = re.sub(r'[^a-zA-Z0-9]', '', word)                #removing special characters
-if len(word)>45 or len(word)==0 :
+#using wl-clipboard for wayland and xclip for x11...
+this_session=subprocess.check_output("echo $XDG_SESSION_TYPE",shell=True,text=True)
+if this_session=="wayland\n":
+    cmd="wl-paste --primary"
+else:
+    cmd="xclip -sel p -o"
+input_word=subprocess.check_output(cmd,shell=True,text=True)
+word = re.sub(r'[^a-zA-Z0-9]', '', "".join(input_word.splitlines()).strip())            #removing special characters,blank spaces and newline
+if len(word)>50 or len(word)==0 :
     exit()
 
 
