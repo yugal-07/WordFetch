0c60d0ee2b9973cfc5afb79c7eacb84aae6c6654
yugal <patilyugal2007@gmail.com>
2026-07-09 16:16:28 +0530

    added detection of primary selection to avoid copying

--- dictionary.py
+++ dictionary.py
@@ -1,12 +1,15 @@
-import pyperclip,asyncio,requests,re
+import asyncio,requests,re,subprocess
 from desktop_notifier import DesktopNotifier
 
-#reading data from clipboard...
-word=pyperclip.paste()
-word="".join(word.splitlines()).strip()         #removing \n and blank spaces
-word = re.sub(r'[^a-zA-Z0-9]', '', word)        #removing special characters
+#reading data from primary selection...
+#using wl-clipboard for wayland
+output_from_sp=subprocess.run(["wl-paste","--primary"],capture_output=True, text=True)
+input_word=str(output_from_sp.stdout)
+word="".join(input_word.splitlines()).strip()           #removing \n and blank spaces
+word = re.sub(r'[^a-zA-Z0-9]', '', word)                #removing special characters
 if len(word)>45 or len(word)==0 :
     exit()
+
 
 #dictionary api integration
 link=f"https://api.dictionaryapi.dev/api/v2/entries/en/{word}"
@@ -17,16 +20,16 @@ for i in data:
     if i=='title':
         found=True
         break
-
 if found==True:
     meaning=data['title']
 else:
     meaning=data[0]['meanings'][0]['definitions'][0]['definition']
 
+
 #notify
 notifier = DesktopNotifier()
 
 async def main():
-    await notifier.send(title="Live Dictionary", message=meaning)
+    await notifier.send(title=input_word, message=meaning)
 
 asyncio.run(main())
